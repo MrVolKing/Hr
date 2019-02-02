e7280e3159e85fad28eb5b3092c5b1f08e00e019
mrKing <MrKing>
2019-02-02 12:24:22 +0200

    first commit

---
+++ src/scripts/common/custom.js
@@ -0,0 +1,40 @@
+$(document).ready(() => {
+
+    $(document).on('click', '.sectmenu__item a', function() {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 500);
+    })
+
+    // burger
+    $('.burger').click(function() {
+        $('.adaptmenu').addClass('adaptmenu_active');
+        $('body').css('overflow', 'hidden');
+    });
+
+    $('.close').click(function() {
+        $('.adaptmenu').removeClass('adaptmenu_active');
+        $('body').css('overflow', 'visible');
+        $('body').css('overflow-x', 'hidden');
+    });
+
+    $('.adaptmenu nav').click(function() {
+        $('.adaptmenu').removeClass('adaptmenu_active');
+        $('body').css('overflow', 'visible');
+        $('body').css('overflow-x', 'hidden');
+    });
+
+
+});
+
+                                                                        
+
+
+
+                                                                           
+
+                                        
+
